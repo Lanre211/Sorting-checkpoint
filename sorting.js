df38038b27fae6c86044260e644b12526ec1e3cd
Lanre <olawunmilanre00@gmail.com>
2023-07-01 22:23:00 +0100

    newsort

--- sorting.js
+++ sorting.js
@@ -1,4 +1,3 @@
-# Sorting-checkpoint
 // problem 1
 // take in three cards namely card 1, card 2, card 3
 // assign the cards to an array
@@ -6,8 +5,23 @@
 // the new card should be placed after the last index of the array
 
 
+let cardarr = ("1,2,3")
+
+console.log(cardarr);
+
+ i = cardarr.length [i+1]
+
 
 //problem 2
 // compare i to the remaining index 
 // if i is greater than the next index
 // swap i with the next index
+
+function Newcard(insert) {
+  For 
+    if(cardarr[i] > cardarr[i+1]);
+    
+    THEN
+    
+    swap (i, cardarr.length[i+1]);
+}
